048639899843cd652c37aac6c85e1cea8d39b46c
Administrador@ASEGIN.local <Administrador@ASEGIN.local>
2019-07-13 20:45:22 -0500

    prototipo de landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { InformationComponent } from './components/information/information.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    InformationComponent,
+    ProductosComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
